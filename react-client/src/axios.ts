f9d93af854e7e9d2d20b80694cad16b6dc862360
Kirill-Bokov <kirill37reg2@gmail.com>
2025-05-12 17:52:44 +0300

    made env files

--- react-client/src/axios.ts
+++ react-client/src/axios.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_URL
+
+console.info(`[axios] API baseURL = ${baseURL}`)
+
 const apiClient = axios.create({
-  baseURL: 'http://localhost:4001',
+  baseURL,
 })
 
 export default apiClient
